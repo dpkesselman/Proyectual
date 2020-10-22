e855ede7643f8de5221bee5b31af472c48a22b3b
dpkesselman <d.p.kesselman@gmail.com>
2020-10-21 22:02:56 -0300

    Se agrega el ejercicio 11 al ejercicio 9

--- src/EjNueve.java
+++ src/EjNueve.java
@@ -7,21 +7,33 @@ public class EjNueve {
 	Scanner sc = new Scanner(System.in);
 	System.out.println("Ingrese la cantidad de números enteros que desee y luego un número negativo para finalizar");
 	long acumulador = 0;
-	int maximo = 0;
+	//int maximo = 0;
+	//int minimo = 0;
+	int maximo;
+	int minimo;
 	int num;
 	num = sc.nextInt();
+	
+	maximo = num;
+	minimo = num;
 	
 	while (num > 0) {
 		acumulador = acumulador + num;
 		if (num > maximo)
 		{
-			maximo = num;
+			maximo = num;			
+		}
+		if (num < minimo)
+		{
+			minimo = num;
 		}
 		num = sc.nextInt();
 	}
 	
+	
 	System.out.println("La suma es " + acumulador);
-	System.out.println("El máximo es " + maximo);
+	System.out.println("El número más grande es " + maximo);
+	System.out.println("El número más chico es " + minimo);
 
 }
 }
